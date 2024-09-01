b01fb2a98d46544fab4fd5adf3518b17316c6b50
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-01 18:26:30 +0200

    add pages, lateralMenu  and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,27 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bookings from "./pages/Bookings";
 import Rooms from "./pages/Rooms";
-import Users from "./pages/Users";
-import Contact from "./pages/Contact";
+import Guest from "./pages/Guest";
+import GuestDetail from "./pages/GuestDetail";
+import Contact from "./pages/Concierge";
 import Dashboard from "./pages/Dashboard";
-import Layout from "./Layout";
+import Layout from "./pages/components/Layout";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="bookings" element={<Bookings />} />
           <Route path="contact" element={<Contact />} />
           <Route path="rooms" element={<Rooms />} />
-          <Route path="users" element={<Users />} />
-        </Routes>
+          <Route path="guest" element={<Guest />} />
+          <Route path="guestDetail" element={<GuestDetail />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/components/LateralMenu/LateralMenu.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./__lateralMenu.scss";
+
+const LateralMenu = () => {
+  return (
+    <div className="lateralMenu">
+      <div className="lateralMenu__sections">
+        <Link to="/" className="lateralMenu__sections__pack">
+          <p>Dashboard</p>
+        </Link>
+        <Link to="/rooms" className="lateralMenu__sections__pack">
+          <p>Rooms</p>
+        </Link>
+        <Link to="/bookings" className="lateralMenu__sections__pack">
+          <p>Bookings</p>
+        </Link>
+        <Link to="/guest" className="lateralMenu__sections__pack">
+          <p>Guest</p>
+        </Link>
+        <Link to="/concierge" className="lateralMenu__sections__pack">
+          <p>Concierge</p>
+        </Link>
+      </div>
+      <div className="lateralMenu__footerLateralMenu">
+        <p>Travl Hotel Admin Dashboard</p>
+        <p>@2020 All Right Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default LateralMenu;
