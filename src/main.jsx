5de36ba82970982ff985ef30a89a44ad828b9514
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-04 12:58:35 +0200

    router - add others routes

--- src/main.jsx
+++ src/main.jsx
@@ -16,11 +16,29 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="concierge" element={<Concierge />} />
-          <Route path="rooms" element={<Rooms />} />
+          <Route path="bookings" element={<Bookings />}>
+            <Route path=":id" element={<Bookings />} />
+            <Route path="new" element={<Bookings />} />
+            <Route path=":id/edit" element={<Bookings />} />
+            <Route path=":id/delete" element={<Bookings />} />
+          </Route>
+          <Route path="concierge" element={<Concierge />}>
+            <Route path=":id" element={<Concierge />} />
+            <Route path="new" element={<Concierge />} />
+            <Route path=":id/edit" element={<Concierge />} />
+            <Route path=":id/delete" element={<Concierge />} />
+          </Route>
+          <Route path="rooms" element={<Rooms />}>
+            <Route path=":id" element={<Rooms />} />
+            <Route path="new" element={<Rooms />} />
+            <Route path=":id/edit" element={<Rooms />} />
+            <Route path=":id/delete" element={<Rooms />} />
+          </Route>
           <Route path="guest" element={<Guest />}>
             <Route path=":id" element={<Guest />} />
+            <Route path="new" element={<Guest />} />
+            <Route path=":id/edit" element={<Guest />} />
+            <Route path=":id/delete" element={<Guest />} />
           </Route>
         </Route>
       </Routes>
