b7299dd1cd4b9a93165e44e6e5e3055d79a859ca
Alex E. C. <alejandro.garcia.carmona@gmail.com>
2024-09-24 20:44:36 +0200

    usersSlice done

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -10,6 +10,7 @@ import {
 const initialState = {
   users: [],
   filteredUsers: [],
+  searchText: "",
   status: promiseStatus.IDLE,
   error: null,
 };
@@ -17,20 +18,43 @@ const initialState = {
 const usersSlice = createSlice({
   name: "users",
   initialState,
-  reducers: {},
+  reducers: {
+    filterUsersByStatus: (state, action) => {
+      const status = action.payload;
+      state.filteredUsers = state.users.filter(
+        (user) => user.status === status || status === "All"
+      );
+    },
+    searchUserByName: (state, action) => {
+      const searchText = action.payload.toLoweCase();
+      state.filteredUsers = state.users.filter((user) =>
+        user.nme.toLoweCase().includes(searchText)
+      );
+    },
+    sortUsersByColumn: (state, action) => {
+      const { column, direction } = action.payload;
+      state.filteredUsers = [...state.filteredUsers].sort((a, b) => {
+        if (a[column] < b[column]) return direction === "asc" ? -1 : 1;
+        if (a[column] > b[column]) return direction === "asc" ? 1 : -1;
+        return 0;
+      });
+    },
+  },
   extraReducers: (builder) => {
     builder
-    .addCase(fetchUserss.pending, (state) => {
-      pending(state);
-    })
-    .addCase(fetchUserss.fulfilled, (state, action) => {
-      changeStatus(state, promiseStatus.FULFILLED);
-      state.users = action.payload;
-    })
-    .addCase(fetchUserss.rejected, (state, action) => {
-      rejected(state, action);
-    });
+      .addCase(fetchUserss.pending, (state) => {
+        pending(state);
+      })
+      .addCase(fetchUserss.fulfilled, (state, action) => {
+        changeStatus(state, promiseStatus.FULFILLED);
+        state.users = action.payload;
+      })
+      .addCase(fetchUserss.rejected, (state, action) => {
+        rejected(state, action);
+      });
   },
 });
 
+export const { filterUsersByStatus, searchUserByName, sortUsersByColumn } =
+  usersSlice.actions;
 export default usersSlice.reducer;
